eade4aeb72bd0e19ed44b78e4889d992cd2855dc
Soumya Mohapatra <soumya.mohapatra@additive-net.de>
2025-07-15 20:53:42 +0200

    Adding rust files

---
+++ rust/dtypes.rs
@@ -0,0 +1,5 @@
+/*
+ * in rust there are two datatype subsets: scalar and compound
+ * rust is statically typed, so it needs to know the datatype of all variables at compile time
+ * while it can infer the type in most cases based on the value, it is a better practice to type annotate
+ */
